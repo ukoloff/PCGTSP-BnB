fbde1c57a5c1d5780165d885c556e73f1d60d48e
Stas Ukolov <ukoloff@gmail.com>
2021-05-12 14:21:51 +0500

    Fail to find cycle cover in undirected graph

---
+++ playground/ap.py
@@ -0,0 +1,28 @@
+#
+# (Undirected) Assignment Problem
+#
+import networkx as nx
+
+import points
+
+
+def ap(graph):
+    bi = nx.Graph()
+    for u, v, w in graph.edges.data('weight'):
+        # print(x)
+        bi.add_edge((1, u), (2, v), weight=w)
+        bi.add_edge((2, u), (1, v), weight=w)
+    print(bi)
+    edges = nx.min_edge_cover(bi)
+    # print(edges)
+    res = nx.create_empty_copy(graph)
+    for (p, u), (q, v) in edges:
+      res.add_edge(u, v)
+    return res
+
+
+if __name__ == '__main__':
+    z = points.pays(7)
+    w = ap(z)
+    with open("tmp/ap.html", "w") as f:
+        print(points.v2svg(w, edges=True, earth=True, wrap=True), file=f)
