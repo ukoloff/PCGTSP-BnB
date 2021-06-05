4301e4076f269c0af197b1b88d7f3c54c5f31039
Stas Ukolov <ukoloff@gmail.com>
2021-06-05 21:30:16 +0500

    Calculate LB via NC+AP

--- playground/salmanize.py
+++ playground/salmanize.py
@@ -1,10 +1,10 @@
 #
 # "Чистый" код для подсчёта пути по "внешнему" пути (суффиксу)
-# NC + APa
+# NC + AP
 import networkx as nx
 
 
-def nc0(dists: nx.DiGraph, clusters: dict, order: nx.DiGraph):
+def nc0(dists: nx.DiGraph, clusters: dict, order: nx.DiGraph, start_cluster=1):
     """Построить граф кластеров для данной задачи
     (с частичным учётом ограничений предшествования)
     """
@@ -16,7 +16,7 @@ def nc0(dists: nx.DiGraph, clusters: dict, order: nx.DiGraph):
         for B in tree:
             if A is B:
                 continue
-            if B == 1:
+            if B == start_cluster:
                 if tree.out_degree(A) != 0:
                     continue
             else:
@@ -34,10 +34,55 @@ def nc0(dists: nx.DiGraph, clusters: dict, order: nx.DiGraph):
     return res
 
 
+def subgraph(nc0: nx.DiGraph, sigma, last_cluster, start_cluster=1):
+    """Выкинуть из графа кластеров все кластеры префикса
+    (кроме первого и последнего)
+    """
+    result = nc0.copy()
+    if len(sigma) <= 1:
+        return result
+    result.remove_nodes_from(
+        n for n in sigma if last_cluster != n != start_cluster)
+    result.remove_edges_from((start_cluster, n)
+                             for n in list(result.successors(start_cluster)))
+    result.remove_edges_from((n, last_cluster)
+                             for n in list(result.predecessors(last_cluster)))
+    if len(sigma) < len(nc0):
+        # Not a leaf node
+        try:
+            result.remove_edge(last_cluster, start_cluster)
+        except nx.NetworkXError:
+            pass
+    result.add_edge(start_cluster, last_cluster, weight=0)
+    return result
+
+
+def AP(graph: nx.DiGraph):
+    """Рассчитать LB-оценку методом AP / Matching
+    """
+    bi = nx.Graph()
+    for u, v, w in graph.edges.data('weight'):
+        bi.add_edge((1, u), (2, v), weight=w)
+    matching = nx.algorithms.bipartite.matching.minimum_weight_full_matching(bi,
+                                                                             top_nodes=((1, n) for n in graph))
+    return sum(graph[u][v]['weight'] for (p, u), (q, v) in matching.items() if p == 1 and q == 2)
+
+
+def lower_bound(nc0: nx.DiGraph, sigma, last_cluster, start_cluster=1):
+    """Рассчитать LB для "внешнего" графа
+    """
+    return AP(subgraph(nc0, sigma, last_cluster, start_cluster))
+
+
 if __name__ == '__main__':
     import samples
+    from salmanize_test import ex5s
 
     # task = samples.random(27, 7)
     task = samples.load("e5x_1")
     cg = nc0(task.dists, task.clusters, task.tree)
-    print(cg)
+    for sigma, ap in ex5s.items():
+        LB = lower_bound(cg, sorted(sigma), sigma[-1], sigma[0])
+        if LB != ap:
+            print("Error for", sigma)
+    print("Tested:", len(ex5s), "prefixes")
