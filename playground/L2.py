1b27d35ff2fe36560f7f8303651389abb5ebde46
Stas Ukolov <ukoloff@gmail.com>
2021-06-08 17:13:20 +0500

    Build L2

--- playground/L2.py
+++ playground/L2.py
@@ -3,6 +3,7 @@
 # (расстояния между кластерами через промежуточный кластер)
 #
 import enum
+from re import S
 import numpy as np
 import networkx as nx
 from numpy.lib import ndenumerate
@@ -44,14 +45,39 @@ def L2(task: Task, start_cluster=1):
         D = np.full((len(clusters[A]), len(clusters[B])), np.inf)
         for (p, q), _ in ndenumerate(D):
           if dists.has_edge(clusters[A][p], clusters[B][q]):
-            D[p, q] = dists.edges[clusters[A][p], clusters[B][q]]['weight']
+            D[p, q] = dists.edges[clusters[A][p], clusters[B][q]]['weight'] / 2
         c2p[cid[A], slice(*crgs[B])] = D.min(axis=0)
         p2c[slice(*crgs[A]), cid[B]] = D.min(axis=1)
-    print(c2p)
-    print(p2c)
+
+    # Отделим расстояния для стартового кластера
+    c2p1 = c2p[:, slice(*crgs[start_cluster])]
+    p2c1 = p2c[slice(*crgs[start_cluster]), :]
+    di_via_start = np.min(c2p1[..., None] + p2c1[None, ...], axis=1)
+
+    c2p = np.delete(c2p, slice(*crgs[start_cluster]), axis=1)
+    p2c = np.delete(p2c, slice(*crgs[start_cluster]), axis=0)
+    di = np.min(c2p[..., None] + p2c[None, ...], axis=1)
+
+    # Строим граф L2
+    res = nx.DiGraph()
+    rez = np.full((len(clusters), len(clusters)), np.inf)
+    for A in clusters:
+      for B in clusters:
+        if A is B:
+          continue
+        w = np.inf
+        if A == start_cluster or B == start_cluster or not task.tree_closure.has_edge(B, A):
+          w = di[cid[A], cid[B]]
+        if not task.tree_closure.has_edge(A, B):
+          w = min(w, di_via_start[cid[A], cid[B]])
+        res.add_edge(A, B, weight=w)
+        rez[cid[A], cid[B]] = w
+    task.L2 = res
+
 
 if __name__ == '__main__':
   import samples
 
-  z = samples.random(27, 7)
+  # z = samples.random(27, 7)
+  z = samples.load("e5x_1")
   L2(z)
