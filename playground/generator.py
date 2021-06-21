2a681436b0ab84c1387e27e89622f55788f8ce67
Stas Ukolov <ukoloff@gmail.com>
2021-06-21 12:28:09 +0500

    Build suffix graphs one by one

--- playground/generator.py
+++ playground/generator.py
@@ -6,6 +6,7 @@ import sys
 from klasses import Task, STNode
 import samples
 import nc
+import children
 
 src = "e5x_1" if len(sys.argv) < 2 else sys.argv[1]
 print("Loading:", src)
@@ -17,3 +18,15 @@ print("Size:\t", task.dists.order(), '/', len(task.clusters))
 print("UB:\t", task.UB)
 # print("Solution so far:\t", task.solution)
 print()
+
+nc.initL1(task)
+nc.initL2(task)
+
+root = STNode(task)
+for node in children.subtree(root):
+    print(node.sigma, end='\t', flush=True)
+
+    graph = nc.nc(node, L=1)
+    tree = nc.get_order(node, graph)
+    # Use them!
+    print(len(graph), len(tree))
