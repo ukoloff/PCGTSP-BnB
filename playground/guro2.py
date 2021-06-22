aa688d06402c44fa7494690a72df8c5f46d52896
Stas Ukolov <ukoloff@gmail.com>
2021-06-22 23:13:19 +0500

    Finish Gurobi approach to model building

--- playground/guro2.py
+++ playground/guro2.py
@@ -7,6 +7,7 @@ import networkx as nx
 import gurobipy as gp
 from gurobipy import GRB
 
+
 def model(graph: nx.DiGraph, tree_closure: nx.DiGraph, start_node=1):
     m = gp.Model('PCATSPxy')
     m.Params.LogToConsole = 0
@@ -16,8 +17,12 @@ def model(graph: nx.DiGraph, tree_closure: nx.DiGraph, start_node=1):
     iVert = {v: i for i, v in enumerate(vertices)}
     start_idx = iVert[start_node]
 
-    Xs, costs = gp.multidict(((iVert[u], iVert[v]), w) for u, v, w in graph.edges.data('weight') if u != v)
-    Ys = gp.tuplelist((u, v) for u in range(n) if u != start_idx for v in range(n) if v != start_idx if u != v)
+    Xs, costs = gp.multidict(
+        ((iVert[u], iVert[v]), w)
+        for u, v, w in graph.edges.data('weight') if u != v)
+    Ys = gp.tuplelist(
+        (u, v)
+        for u in range(n) if u != start_idx for v in range(n) if v != start_idx if u != v)
 
     # VARIABLES
     x = m.addVars(Xs, vtype=GRB.BINARY, name='x')
@@ -31,46 +36,33 @@ def model(graph: nx.DiGraph, tree_closure: nx.DiGraph, start_node=1):
     m.addConstrs((x.sum(i, '*') == 1 for i in range(n)), 'out')
     m.addConstrs((x.sum('*', i) == 1 for i in range(n)), 'in')
 
-    m.write('!!!.lp')
-    return m
-
     # SUB-TOUR ELIMINATION
-    for u, v in graph.edges:
-        iu = iVert[u]
-        iv = iVert[v]
-
-        if start_idx in (iu, iv):
-            continue
-
-        m.addConstr(
-            y[iu, iv] - x[iu, iv] >= 0,
-            f'yx_{iu}_{iv}')
-        m.addConstr(
-            y[iu, iv] + y[iv, iu] == 1,
-            f'yy_{iu}_{iv}'
-        )
-
-        if iu >= iv:
-            continue
-        for w in set(graph.predecessors(u)) & set(graph.successors(v)):
-            iw = iVert[w]
-            if iw == start_idx:
-                continue
-            if iu >= iw:
-                continue
-            m.addConstr(
-                y[iu, iv] + y[iv, iw] + y[iw, iu] <= 2,
-                f'tri_{iu}_{iv}_{iw}')
+    m.addConstrs(
+        (y[u, v] - x[u, v] >= 0
+         for u, v in x if u != start_idx if v != start_idx),
+        'xy')
+    m.addConstrs(
+        (y[u, v] + y[v, u] == 1
+         for u in range(n) if u != start_idx for v in range(u + 1, n) if v != start_idx),
+        'yy')
+    m.addConstrs(
+        (y[a, b] + y[b, c] + y[c, a] <= 2
+         for a in range(n) if a != start_idx
+         for b in range(a + 1, n) if b != start_idx
+         for c in range(a + 1, n) if c != start_idx
+         if b != c),
+        'tri')
 
     # PRECEDENCE CONSTRAINTS
-    for u, v in tree_closure.edges:
-        if u not in iVert or v not in iVert:
-          continue
-        iu = iVert[u]
-        iv = iVert[v]
-        m.addConstr(
-            y[iu, iv] == 1,
-            f'pc_{iu}_{iv}')
+    m.addConstrs(
+        (y[u, v] == 1
+         for u, v in (
+            (iVert[u], iVert[v])
+            for u, v in tree_closure.edges
+            if u in iVert and v in iVert)
+         if u != start_idx and v != start_idx
+         ),
+        'pc')
 
     return m
 
@@ -95,20 +87,23 @@ if __name__ == '__main__':
     for v in m.getVars():
         if not v.VarName.startswith('x[') or v.X == 0:
             continue
-        print(v.VarName[2:-1], end='\t')
+        print(v.VarName[2:-1], end=' ')
 
     # Time it!
     print()
     build = timeit(lambda: model(graph, task.tree_closure), number=10) / 10
     print(f'Build: {build * 1000:.3f}ms')
 
-    solve = timeit(lambda: model(graph, task.tree_closure).optimize(), number=10) / 10
+    solve = timeit(lambda: model(
+        graph, task.tree_closure).optimize(), number=10) / 10
     print(f'Build + Solve: {solve * 1000:.3f}ms')
 
     from guro2x import create_model
     print('[guro2x]')
-    build = timeit(lambda: create_model(graph, task.tree_closure), number=10) / 10
+    build = timeit(lambda: create_model(
+        graph, task.tree_closure), number=10) / 10
     print(f'Build: {build * 1000:.3f}ms')
 
-    solve = timeit(lambda: create_model(graph, task.tree_closure)[0].optimize(), number=10) / 10
+    solve = timeit(lambda: create_model(graph, task.tree_closure)[
+                   0].optimize(), number=10) / 10
     print(f'Build + Solve: {solve * 1000:.3f}ms')
