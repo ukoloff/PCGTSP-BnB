f70a5cdeafa3690bbd492060fdd8f515981fe0c1
Stas Ukolov <ukoloff@gmail.com>
2021-06-29 16:35:36 +0500

    Report on logs

--- playground/dp/report.py
+++ playground/dp/report.py
@@ -7,7 +7,14 @@ from pathlib import Path
 logs = Path(__file__).parent.parent.parent / "logs/dp"
 for log in logs.iterdir():
   time = None
+  states, sigmas = 0, 0
+  LBs = []
   for line in log.open():
-    if m := re.match("Elapsed\s+time:\s+(\d+([.]\d*))", line):
+    if m := re.match(r"Elapsed\s+time:\s+(\d+([.]\d*)?)", line):
       time = float(m[1])
-  print(log.stem, time, sep='\t')
+    if m := re.match("layer\s+\d+\s+of\ssize\s+(\d+)\s+\((\d+)\)", line):
+      states += int(m[1])
+      sigmas += int(m[2])
+    if m := re.search(r"Best\s+LB\s+is\s+(\d+([.]\d+)?)", line):
+      LBs.append(float(m[1]))
+  print(log.stem, time, states, sigmas, LBs, sep='\t')
