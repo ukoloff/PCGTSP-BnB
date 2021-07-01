90eadd6103879293785b1965c6a451602d8166fb
Stas Ukolov <ukoloff@gmail.com>
2021-07-01 10:43:44 +0500

    Write shell script

--- playground/experiment/build.py
+++ playground/experiment/build.py
@@ -6,11 +6,50 @@ root = Path(__file__)
 for i in range(3):
   root = root.parent
 
+samples = root / "Salman"
+tasks = samples / "input"
+logs = root / "logs/salman"
+logs.mkdir(parents=True, exist_ok=True)
+
 sources = []
-for f in (root / "Salman/input").glob('*'):
+for f in tasks.glob('*'):
     for line in open(f):
       if m := re.match(r"\s*GTSP_SETS\s*:\s*(\d+)", line):
         sources.append((int(m[1]), f.stem))
 
 sources = sorted(sources)
-print(*sources)
+# print(*sources)
+
+def UB(sample):
+  for line in (samples / "heuristic" / f"{sample}.pcglns.result.txt").open():
+    if m := re.match(r"\s*Cost\s*:\s*(\d+)", line):
+      return float(m[1])
+
+def header():
+  print("""
+#!/bin/bash
+
+export GRB_LICENSE_FILE=~/gurobi.lic
+export LD_PRELOAD=~/ioctl/ioctl_faker.so
+
+threads=16
+m="--mem=128G"
+g="--cpus-per-task=$threads"
+t="--time=600"
+p="--partition=apollo"
+mode="-mode=default"
+#max_time="-max_time=10800"
+
+source ../../vi/mine/bin/activate
+""".strip())
+
+header()
+for n, f in sources:
+  thisUB = UB(f)
+  print(f"""
+# {f}[{n}] UB={thisUB}
+outf="--output={logs}/{f}.log.txt"
+errf="--error={logs}/{f}.errors.log.txt"
+cmd="srun $m $t $g $p -u python DP_pcglns.py -i={tasks}/{f}.pcglns -w=$threads -UB={thisUB}"
+sbatch $t $m $p $errf $outf $g -J {f} --wrap="$cmd"
+""")
